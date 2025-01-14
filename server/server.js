223e5c095e9ffd3e07c0d84197fc434fe0634cef
Pradh123 <vermapradhumn3@gmail.com>
2025-01-14 15:10:16 +0530

    erfg

---
+++ server/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import http from 'http'
+import router from '../routes/imageUpload.js';
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Enable CORS
+app.use(cors());
+
+// Use image upload routes
+app.use('/images', router);
+app.get("/login",(req,res)=>{
+    res.send("very nice")
+})
+// Start the server
+const servermodule= (req, res) => {
+  const server = http.createServer(app);
+  server.emit('request', req, res);
+};
+export default servermodule
