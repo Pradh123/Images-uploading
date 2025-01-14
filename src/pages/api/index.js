6e88796cbe3958ec736f5564edbfc4af1923a2e4
Pradh123 <vermapradhumn3@gmail.com>
2025-01-14 12:33:00 +0530

    pradhumn verma

--- src/pages/api/index.js
+++ src/pages/api/index.js
@@ -1,7 +1,6 @@
  // Import the Express app
 import { createServer } from 'http';
 import app from '../../../server'
-app
 const app1= (req, res) => {
   const server = createServer(app);
   server.emit('request', req, res);
